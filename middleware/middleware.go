702568aa1812c536bd44b9444d31f6f33cbf7e91
Karim Rahman Hartono <karim.rahman@bukalapak.com>
2021-09-22 17:22:52 +0700

    add comment

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -18,13 +18,14 @@ func Middleware(handle Handler) httprouter.Handle {
 
 		start := time.Now()
 		query := r.URL.Query()
-		// body := []byte{}
-		// if r.Body != nil {
-		// 	defer r.Body.Close()
-		// 	body, _ = ioutil.ReadAll(r.Body)
-		// 	r.Body = ioutil.NopCloser(bytes.NewBuffer(body))
-		// }
+		/* body := []byte{}
+		if r.Body != nil {
+			defer r.Body.Close()
+			body, _ = ioutil.ReadAll(r.Body)
+			r.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+		} */
 
+		// this executes the methods in handler!
 		err := handle(w, r, params)
 
 		elapsed := time.Since(start).Seconds() * 1000
@@ -36,8 +37,8 @@ func Middleware(handle Handler) httprouter.Handle {
 				"method": r.Method,
 				"path":   r.URL.Path,
 				"query":  query.Encode(),
-				// "body":   string(body),
-			}).Warning(err.Error())
+				//"body":   string(body),
+			}).Warning(err.Error()) // this is where the 'msg' comes from
 		} else {
 			log.WithFields(log.Fields{
 				"time":   elapsedStr,
